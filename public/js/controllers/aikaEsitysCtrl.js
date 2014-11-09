5c7882b3991add4fbd3bc1cfdcf41ea85480a42e
TeroS <teros@mail.com>
2014-11-09 22:34:01 +0200

    lisätty aikaesitys logiikka

--- public/js/controllers/aikaEsitysCtrl.js
+++ public/js/controllers/aikaEsitysCtrl.js
@@ -1,15 +1,38 @@
-var aikaEsitysCtrl = function ($scope) {
+var aikaEsitysCtrl = function ($scope, $filter) {
     
-  $scope.today = function() {
-    $scope.dt = new Date();
+  //var prevDate;
+  //$scope.minDate = $scope.minDate ? null : new Date();
+    
+  /*$scope.today = function() {
+    $scope.dt = ''; //new Date();
   };
-  $scope.today();
+  $scope.today();*/
+  $scope.dateList = {};
 
   $scope.toggleMin = function() {
     $scope.minDate = $scope.minDate ? null : new Date();
+    //$scope.count = 0;
+    //$scope.prevDate = '';
   };
   $scope.toggleMin();
-
+  
+  $scope.addDate = function(dateData) {
+      tmpDate = $filter('date')(dateData, 'longDate');
+      //lisää pvm property objektiin, jos on klikattu validia pvm:ää (imitoi settiä)
+      if (typeof dateData !== "undefined") {
+          $scope.dateList[tmpDate] = true;
+      }
+      //console.log($scope.dateList);
+      //syötä objektin avaimet(=valitut päivämäärät) listaan
+      var avaimet = [];
+      for(var prop in $scope.dateList) {
+         avaimet.push(prop); 
+      }
+      console.log(avaimet);
+      
+  };
+  
+ 
  /* $scope.open = function($event) {
     $event.preventDefault();
     $event.stopPropagation();
