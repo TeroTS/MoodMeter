90f4174a052bb00ee1dd44f7035522b214084403
Tero Suhonen <tero.suhonen@comiq.fi>
2014-12-07 00:42:41 +0200

    GET and POST rest API working

--- app/routes.js
+++ app/routes.js
@@ -1,4 +1,4 @@
-
+var mongoose = require('mongoose');
 var Poll = require('./models/poll');
 
 module.exports = function(app) {
@@ -8,23 +8,29 @@ module.exports = function(app) {
     // authentication routes
 
     // sample api route
- /*   app.get('/api/nerds', function(req, res) {
+    app.get('/rest/polls', function(req, res) {
         // use mongoose to get all nerds in the database
-        Nerd.find(function(err, nerds) {
+        Poll.find(function(err, polls) {
 
             // if there is an error retrieving, send the error. 
                             // nothing after res.send(err) will execute
             if (err)
                 res.send(err);
 
-            res.json(nerds); // return all nerds in JSON format
+            res.json(polls); // return all nerds in JSON format
         });
-    });*/
+    });
         
     // route to handle creating goes here (app.post)
     app.post('/rest/polls', function(req, res) {
+    	
+    	  //generate objectid for unique poll url
+    	  var objId = mongoose.Types.ObjectId();
+    	  var pollUrlString = objId.toString();
+    	  var adminUrlString = pollUrlString + '/admin';
 
-        Todo.create({
+        Poll.create({
+        		_id : objId,
             title : req.body.otsikko,
             location : req.body.sijainti,
             description : req.body.kuvaus,
@@ -33,13 +39,14 @@ module.exports = function(app) {
             dates : req.body.pvm,
             participantEmails : req.body.osallistujatSposti,
             participants : req.body.osallistujat,
-            adminUrl : req.body.hallinnointiUrl,
-            pollUrl : req.body.kyselyUrl
+            adminUrl : adminUrlString,
+            pollUrl : pollUrlString
         }, function(err, todo) {
             if (err)
                 res.send(err);
            }
         );
+        res.json({ message: 'Poll created !' });
     });   
     // route to handle delete goes here (app.delete)
 
