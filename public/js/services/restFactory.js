9f3518aa1daad66aabf96d0ef8e41d2c3c07de37
Tero Suhonen <tero.suhonen@comiq.fi>
2014-12-07 00:42:41 +0200

    GET and POST rest API working

--- public/js/services/restFactory.js
+++ public/js/services/restFactory.js
@@ -24,3 +24,26 @@
     //restFactory.$inject = ['$http'];
         
     angular.module('roodleApp').factory('restFactory', restFactory);
+    
+   /* angular.module('roodleApp').factory('restFactory', function ($http) { 
+        var factory = {};
+        
+        factory.getPoll = function(pollId) {
+            return $http.get('/rest/polls/' + pollId);
+        };
+        
+        factory.createPoll = function(data) {
+            return $http.post('/rest/polls', data);
+        };
+        
+        factory.updatePoll = function(data) {
+            return $http.put('/rest/polls', data);
+        };
+        
+        factory.deletepoll = function(pollId) {
+            return $http.delete('/rest/polls/' + pollId);
+        };
+        
+        return factory;    
+    
+    });*/
