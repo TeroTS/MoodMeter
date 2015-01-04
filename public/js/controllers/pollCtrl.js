1fb58840db0203aed531d0567fca949287d03c42
TeroS <teros@mail.com>
2015-01-05 01:46:44 +0200

    Added logic to add poll users

--- public/js/controllers/pollCtrl.js
+++ public/js/controllers/pollCtrl.js
@@ -1,5 +1,8 @@
 var pollCtrl = function ($scope, $routeParams, dataService, restFactory) {
-
+    
+    $scope.users = [];
+    $scope.newUser = {};
+    
     console.log($routeParams.pollId);
     restFactory.getPoll($routeParams.pollId)
         .success(function(data, status, headers, config) {
@@ -11,6 +14,13 @@ var pollCtrl = function ($scope, $routeParams, dataService, restFactory) {
         .error(function(data, status, headers, config) {
             console.log("Error: " + status);
         });
+        
+    $scope.saveData = function() {
+        console.log($scope.newUser);
+        $scope.users.push($scope.newUser); //{'name': $scope.name, 'dates': $scope.dates});
+        $scope.newUser = {};
+        console.log($scope.users);
+    };
   
 };
 
