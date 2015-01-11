96cb55757457f25d5fcb4e5b3e9f945b0fd4535f
TeroS <teros@mail.com>
2015-01-11 02:22:53 +0200

    Refactoring

--- public/js/controllers/pollCtrl.js
+++ public/js/controllers/pollCtrl.js
@@ -3,33 +3,19 @@ var pollCtrl = function ($scope, $routeParams, $location, dataService, restFacto
     $scope.users = [];
     $scope.newUser = {};
     $scope.edit = false;
-    $scope.admin = false;
-    
-    //console.log($routeParams.admin);
-    //admin page
- /*   if (typeof $routeParams.admin != 'undefined') {
-        $scope.admin = true;
-    }*/
     
     restFactory.getPoll($routeParams.pollId)
         .success(function(data, status, headers, config) {
-           // console.log(data);
-           // console.log(status);
             if (data === null) {
                 $location.path("/pollNotFound");
             } else {
                 $scope.poll = data;
-                //console.log(data.participants);
                 if (data.participants.length == 0) {
-                    $scope.users = []; //JSON.parse("[{}]");//data.participants);
+                    $scope.users = [];
                 } else {
                     $scope.users = JSON.parse(data.participants); 
                 }              
-            }
-            //admin page
-          /*  if (typeof $routeParams.admin != 'undefined') {
-                $scope.admin = true;
-            }*/       
+            }  
         })
         .error(function(data, status, headers, config) {
             console.log("Error: " + status);
@@ -52,11 +38,8 @@ var pollCtrl = function ($scope, $routeParams, $location, dataService, restFacto
         }
         $scope.edit = false;
         $scope.newUser = {};
-        console.log($scope.users);
         restFactory.updatePoll($routeParams.pollId, $scope.users)
-        .success(function(data, status, headers, config) {
-            //console.log(data);   
-        })
+        .success(function(data, status, headers, config) {})
         .error(function(data, status, headers, config) {
             console.log("Error: " + status);
         });       
@@ -66,9 +49,7 @@ var pollCtrl = function ($scope, $routeParams, $location, dataService, restFacto
         var index = $scope.users.indexOf(user);
         $scope.users.splice(index, 1); 
         restFactory.updatePoll($routeParams.pollId, $scope.users)
-        .success(function(data, status, headers, config) {
-            //console.log(data);   
-        })
+        .success(function(data, status, headers, config) {})
         .error(function(data, status, headers, config) {
             console.log("Error: " + status);
         }); 
@@ -90,7 +71,6 @@ var pollCtrl = function ($scope, $routeParams, $location, dataService, restFacto
   
     modalInstance.result.then(function () {
       deletePoll();
-      //$location.path("/pollRemoved");
     }, function () {});
  };
   
