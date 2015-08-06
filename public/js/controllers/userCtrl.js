8de8d982cb599334d2bb0761d0e776a449809dd3
Tero Suhonen <tero.suhonen@luukku.com>
2015-08-06 23:15:39 +0300

    Fixed persistent selection value

--- public/js/controllers/userCtrl.js
+++ public/js/controllers/userCtrl.js
@@ -2,33 +2,34 @@ app.controller('userCtrl', function($scope, restFactory, dataService) {
 
     //read user data from persistent object
     var user = dataService.readUserData('data');
-    //console.log(user);
-    $scope.name = user.name;//dataService.readUserData();
-    //$scope.managers = [{name :'manager1'}, {name: 'manager2'}];
-    $scope.myManager = {name : ''};
-    $scope.isManager = false;
-    //is user manager ?
-    var userRole = 'user';
-    //console.log($scope.isManager);
-    //if ($scope.isManager === true) {
-    //  userRole = 'manager';
-    //}
+    var userRole = user.role;
+    $scope.name = user.name;
+    $scope.myManagerName = user.managerName;
+    if (userRole === 'manager')
+      $scope.isManager = true;
+    else
+      $scope.isManager = false;
 
-    //get all managers
+    //get all managers for drop down selection
     restFactory.getManagers()
       .success(function(data, status, headers, config) {
-          $scope.managers = data; //[{name: 'manager1'}, {name: 'manager2'}];//data;
+          $scope.managers = data;
+          //select correct manager object from return data and set it to model
+          for (var i = 0; i < data.length; i++) {
+              if (data[i].name === $scope.myManagerName)
+                  $scope.myManager = data[i];
+          }
       })
       .error(function(data, status, headers, config) {
           console.log("Error: " + status);
       });
 
-    //update manager name
+    //update user data
     $scope.updateUser = function() {
       if ($scope.isManager === true) {
         userRole = 'manager';
+        $scope.myManager.name = "";
       }
-      //console.log($scope.myManager.name);
       restFactory.updateUser(user.id, {role: userRole, manager: $scope.myManager.name})
       .success(function(data, status, headers, config) {
           $scope.user = data;
--- public/js/controllers/usersCtrl.js
+++ public/js/controllers/usersCtrl.js
@@ -6,6 +6,7 @@ app.controller('usersCtrl', function($scope, restFactory, dataService) {
   restFactory.getUsers()
       .success(function(data, status, headers, config) {
           $scope.users = data;
+          console.log(data);
       })
       .error(function(data, status, headers, config) {
           console.log("Error: " + status);
