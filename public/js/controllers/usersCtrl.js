9bf979adb5c94c6d5b31d1bc02404bbb271236bb
Tero Suhonen <tero.suhonen@luukku.com>
2015-09-29 23:22:50 +0300

    UI fixes

--- public/js/controllers/usersCtrl.js
+++ public/js/controllers/usersCtrl.js
@@ -1,4 +1,4 @@
-app.controller('usersCtrl', function($scope, restFactory, dataService) {
+app.controller('usersCtrl', function($scope, $sce, restFactory, dataService) {
 
   //get all users
   restFactory.getUsers()
@@ -16,5 +16,15 @@ app.controller('usersCtrl', function($scope, restFactory, dataService) {
       var userData = $scope.users[idx];
       dataService.writeUserData('data', userData);
   };
+
+  $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  $scope.series = ['Series A', 'Series B'];
+
+  $scope.data = [
+    [65, 59, 80, 81, 56, 55, 40],
+    [28, 48, 40, 19, 86, 27, 90]
+  ];
+
+ $scope.htmlTooltip = $sce.trustAsHtml("<canvas id='bar' class='chart chart-bar' chart-data='[5, 6, 7, 8]' chart-labels='['2006', '2007', '2008', '2009']'></canvas");
 
 });
